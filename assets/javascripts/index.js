ce6362314148fd687fda7bddfa539839477c055c
weemackee <weemackee@gmail.com>
2016-05-23 10:38:19 -0400

    parallax scrolling

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -4,3 +4,33 @@ $( document ).ready(function() {
 	$(".home").fadeIn(1000);
 });
 
+function parallax(){
+	var prxl_lay = document.getElementById('above-fold');
+	prxl_lay.style.top = -(window.pageYOffset/4)+'px';
+};
+
+window.addEventListener("scroll", parallax);
+
+$(window).on('scroll', function(){
+	var offSet = $(document).scrollTop(), opacity = 0;
+	var header = $('header')
+	if(offSet >= $(window).height()){
+		opacity = 1;
+	}else if(offSet < $(window).height()){
+		opacity = offSet / $(window).height();
+	}
+	
+	header.css('opacity',opacity);
+});
+
+$(window).on('scroll', function(){
+	var offSet = $(document).scrollTop(), opacity = 1;
+	var header = $('.home')
+	if(offSet >= $(window).height()){
+		opacity = 0;
+	}else if(offSet < $(window).height()){
+		opacity = 1 - 1.5*(offSet / $(window).height());
+	}
+	
+	header.css('opacity',opacity);
+});
